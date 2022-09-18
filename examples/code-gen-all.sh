99a1e51196a11b46175fa09dbd92325a6a06df85
adam fraser-kruck <adamfk@gmail.com>
2022-09-18 12:53:05 -0500

    examples - add script to update and run all code gen projects

---
+++ examples/code-gen-all.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# This script is intended for maintainers of StateSmith. It allows them to update, 
+# build and run all the code generation for the examples.
+
+# exit when any command fails
+set -e
+
+NUGET_VERSION=0.5.4-alpha
+
+# https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
+EXAMPLES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+cd $EXAMPLES_DIR/BlankTemplate/CodeGen/
+sed -i "s/\"StateSmith\" Version=\".*\"/\"StateSmith\" Version=\"$NUGET_VERSION\"/" CodeGen.csproj
+dotnet build
+dotnet ./bin/Debug/net6.0/CodeGen.dll
+
+cd $EXAMPLES_DIR/Blinky1/CodeGen/
+sed -i "s/\"StateSmith\" Version=\".*\"/\"StateSmith\" Version=\"$NUGET_VERSION\"/" CodeGen.csproj
+dotnet build
+dotnet ./bin/Debug/net6.0/CodeGen.dll
+
+cd $EXAMPLES_DIR/Blinky1Printf/CodeGen/
+sed -i "s/\"StateSmith\" Version=\".*\"/\"StateSmith\" Version=\"$NUGET_VERSION\"/" CodeGen.csproj
+dotnet build
+dotnet ./bin/Debug/net6.0/CodeGen.dll
+
+cd $EXAMPLES_DIR/ButtonSm1Cpp/CodeGen/
+sed -i "s/\"StateSmith\" Version=\".*\"/\"StateSmith\" Version=\"$NUGET_VERSION\"/" CodeGen.csproj
+dotnet build
+dotnet ./bin/Debug/net6.0/CodeGen.dll
+
+cd $EXAMPLES_DIR/LaserTagMenu1/CodeGen/
+sed -i "s/\"StateSmith\" Version=\".*\"/\"StateSmith\" Version=\"$NUGET_VERSION\"/" CodeGen.csproj
+dotnet build
+dotnet ./bin/Debug/net6.0/CodeGen.dll
+
+cd $EXAMPLES_DIR/Tutorial1-blank/CodeGen/
+sed -i "s/\"StateSmith\" Version=\".*\"/\"StateSmith\" Version=\"$NUGET_VERSION\"/" CodeGen.csproj
+dotnet build
+dotnet ./bin/Debug/net6.0/CodeGen.dll
+
+cd $EXAMPLES_DIR/Tutorial1-complete/CodeGen/
+sed -i "s/\"StateSmith\" Version=\".*\"/\"StateSmith\" Version=\"$NUGET_VERSION\"/" CodeGen.csproj
+dotnet build
+dotnet ./bin/Debug/net6.0/CodeGen.dll
+
