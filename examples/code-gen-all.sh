2eabc5f877d78978326d988ab00209812bbb33b3
adam fraser-kruck <adamfk@gmail.com>
2022-09-18 12:54:51 -0500

    examples - tidy code-gen-all.sh

--- examples/code-gen-all.sh
+++ examples/code-gen-all.sh
@@ -11,38 +11,17 @@ NUGET_VERSION=0.5.4-alpha
 # https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
 EXAMPLES_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-cd $EXAMPLES_DIR/BlankTemplate/CodeGen/
-sed -i "s/\"StateSmith\" Version=\".*\"/\"StateSmith\" Version=\"$NUGET_VERSION\"/" CodeGen.csproj
-dotnet build
-dotnet ./bin/Debug/net6.0/CodeGen.dll
+update_and_run_project() {
+    cd $1
+    sed -i "s/\"StateSmith\" Version=\".*\"/\"StateSmith\" Version=\"$NUGET_VERSION\"/" CodeGen.csproj
+    dotnet build
+    dotnet ./bin/Debug/net6.0/CodeGen.dll
+}
 
-cd $EXAMPLES_DIR/Blinky1/CodeGen/
-sed -i "s/\"StateSmith\" Version=\".*\"/\"StateSmith\" Version=\"$NUGET_VERSION\"/" CodeGen.csproj
-dotnet build
-dotnet ./bin/Debug/net6.0/CodeGen.dll
-
-cd $EXAMPLES_DIR/Blinky1Printf/CodeGen/
-sed -i "s/\"StateSmith\" Version=\".*\"/\"StateSmith\" Version=\"$NUGET_VERSION\"/" CodeGen.csproj
-dotnet build
-dotnet ./bin/Debug/net6.0/CodeGen.dll
-
-cd $EXAMPLES_DIR/ButtonSm1Cpp/CodeGen/
-sed -i "s/\"StateSmith\" Version=\".*\"/\"StateSmith\" Version=\"$NUGET_VERSION\"/" CodeGen.csproj
-dotnet build
-dotnet ./bin/Debug/net6.0/CodeGen.dll
-
-cd $EXAMPLES_DIR/LaserTagMenu1/CodeGen/
-sed -i "s/\"StateSmith\" Version=\".*\"/\"StateSmith\" Version=\"$NUGET_VERSION\"/" CodeGen.csproj
-dotnet build
-dotnet ./bin/Debug/net6.0/CodeGen.dll
-
-cd $EXAMPLES_DIR/Tutorial1-blank/CodeGen/
-sed -i "s/\"StateSmith\" Version=\".*\"/\"StateSmith\" Version=\"$NUGET_VERSION\"/" CodeGen.csproj
-dotnet build
-dotnet ./bin/Debug/net6.0/CodeGen.dll
-
-cd $EXAMPLES_DIR/Tutorial1-complete/CodeGen/
-sed -i "s/\"StateSmith\" Version=\".*\"/\"StateSmith\" Version=\"$NUGET_VERSION\"/" CodeGen.csproj
-dotnet build
-dotnet ./bin/Debug/net6.0/CodeGen.dll
-
+update_and_run_project "$EXAMPLES_DIR/BlankTemplate/CodeGen/"
+update_and_run_project "$EXAMPLES_DIR/Blinky1/CodeGen/"
+update_and_run_project "$EXAMPLES_DIR/Blinky1Printf/CodeGen/"
+update_and_run_project "$EXAMPLES_DIR/ButtonSm1Cpp/CodeGen/"
+update_and_run_project "$EXAMPLES_DIR/LaserTagMenu1/CodeGen/"
+update_and_run_project "$EXAMPLES_DIR/Tutorial1-blank/CodeGen/"
+update_and_run_project "$EXAMPLES_DIR/Tutorial1-complete/CodeGen/"
