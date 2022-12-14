7c0a363fbd4cb9e46b171d5b1ab66ed8d9057180
Adam Fraser-Kruck <adamfk@gmail.com>
2022-12-14 01:01:25 -0600

    test - remove BehaviorMatchingFlags and deprecate older methods

--- src/StateSmithTest/VertexTestExtensionMethods.cs
+++ src/StateSmithTest/VertexTestExtensionMethods.cs
@@ -1,25 +1,19 @@
 ﻿using System.Collections.Generic;
 using StateSmith.Compiling;
 using FluentAssertions;
+using System;
 
 namespace StateSmithTest
 {
     public static class VertexTestExtensionMethods
     {
+        [Obsolete($"See {nameof(VertexTestHelper.BuildFluentAssertionBehaviorMatcher)}")]
         public static void ShouldBeExactly(this Behavior behavior, string diagramId = null, Vertex owningVertex = null, Vertex transitionTarget = null,
                     string actionCode = "", string guardCode = "", List<string> triggers = null, double order = Behavior.DEFAULT_ORDER)
         {
             VertexTestHelper.ExpectBehaviorExact(behavior, diagramId: diagramId, owningVertex: owningVertex, transitionTarget: transitionTarget,
                 actionCode: actionCode, guardCode: guardCode, triggers: triggers, order: order);
         }
-
-        public static void ShouldBe(this Behavior behavior, VertexTestHelper matcher, string diagramId = null, Vertex owningVertex = null, Vertex transitionTarget = null,
-                    string actionCode = "", string guardCode = "", List<string> triggers = null, double order = Behavior.DEFAULT_ORDER)
-        {
-            matcher.ExpectBehavior(behavior, diagramId: diagramId, owningVertex: owningVertex, transitionTarget: transitionTarget,
-                actionCode: actionCode, guardCode: guardCode, triggers: triggers, order: order);
-        }
-
 
         public static void ShouldHaveNoIncomingTransitions(this Vertex vertex)
         {
--- src/StateSmithTest/VertexTestHelpers.cs
+++ src/StateSmithTest/VertexTestHelpers.cs
@@ -6,49 +6,8 @@ using FABehaviorMatcherFunc = System.Func<FluentAssertions.Equivalency.Equivalen
 
 namespace StateSmithTest
 {
-    public class BehaviorMatchingFlags
-    {
-        public bool diagramId = false;
-        public bool owningVertex = false;
-        public bool transitionTarget = true;
-        public bool actionCode = true;
-        public bool guardCode = true;
-        public bool triggers = true;
-        public bool order = false;
-    }
-
     public class VertexTestHelper
     {
-        public BehaviorMatchingFlags matchingFlags = new();
-        
-        public void ExpectBehavior(Behavior behavior, string diagramId = null, Vertex owningVertex = null, Vertex transitionTarget = null,
-                    string actionCode = "", string guardCode = "", List<string> triggers = null, double order = Behavior.DEFAULT_ORDER)
-        {
-            if (matchingFlags.diagramId)
-                behavior.DiagramId.Should().Be(diagramId);
-
-            if (matchingFlags.owningVertex)
-                behavior.OwningVertex.Should().Be(owningVertex);
-
-            if (matchingFlags.transitionTarget)
-                behavior.TransitionTarget.Should().Be(transitionTarget);
-
-            if (matchingFlags.actionCode)
-                behavior.actionCode.Should().Be(actionCode);
-
-            if (matchingFlags.guardCode)
-                behavior.guardCode.Should().Be(guardCode);
-
-            if (matchingFlags.triggers)
-            {
-                triggers = triggers ?? new List<string>();
-                behavior.triggers.Should().BeEquivalentTo(triggers);
-            }
-
-            if (matchingFlags.order)
-                behavior.order.Should().Be(order);
-        }
-
         public static FABehaviorMatcherFunc BuildFluentAssertionBehaviorMatcher(bool diagramId = false, bool owningVertex = false, bool transitionTarget = true, bool actionCode = true, bool guardCode = true, bool triggers = true, bool order = false)
         {
             FABehaviorMatcherFunc config
@@ -68,6 +27,7 @@ namespace StateSmithTest
             return config;
         }
 
+        [Obsolete($"See {nameof(VertexTestHelper.BuildFluentAssertionBehaviorMatcher)}")]
         public static void ExpectBehaviorExact(Behavior behavior, string diagramId = null, Vertex owningVertex = null, Vertex transitionTarget = null,
                     string actionCode = null, string guardCode = null, List<string> triggers = null, double order = Behavior.DEFAULT_ORDER)
         {
@@ -82,8 +42,6 @@ namespace StateSmithTest
 
             behavior.order.Should().Be(order);
         }
-
-
 
         public static List<string> TriggerList(params string[] triggers)
         {
