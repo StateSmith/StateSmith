e01188175cf4ac17a7f29095d5f9fbca2fc4821e
adam fraser-kruck <adamfk@gmail.com>
2022-12-17 18:36:29 -0600

    antlr - compile.sh allow running from other directories

--- antlr/compile.sh
+++ antlr/compile.sh
@@ -3,6 +3,8 @@
 # exit when any command fails
 # set -e
 
+cd "$(dirname "$BASH_SOURCE")"  # required because this script is sourced
+
 # ensure to use setup.bash first
 # call like this: . compile.sh
 # see https://stackoverflow.com/questions/9772036/pass-all-variables-from-one-shell-script-to-another
@@ -13,3 +15,5 @@ antlr4 PlantUML.g4 -o out
 antlr4 PlantUML.g4 -Dlanguage=CSharp -visitor -o ../src/StateSmith/Input/PlantUML/antlr4/
 
 javac out/*.java
+
+cd -
