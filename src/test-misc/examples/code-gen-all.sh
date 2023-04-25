0d155972ba508a287ce740699b2bbf2ed655b82d
adam fraser-kruck <adamfk@gmail.com>
2023-04-24 23:34:39 -0500

    examples - move to test directory. Closes #173

--- src/test-misc/examples/code-gen-all.sh
+++ src/test-misc/examples/code-gen-all.sh
@@ -26,10 +26,14 @@ set_project_to_use_nuget() {
 }
 
 set_project_to_use_local_csproj() {
-    sed_update_project_file "<ProjectReference Include=\"../../../src/StateSmith/StateSmith.csproj\" />"
+    sed_update_project_file "<ProjectReference Include=\"../../../../StateSmith/StateSmith.csproj\" />"
 }
 
 update_and_run_project() {
+
+    echo "-----------------------------------------------------------"
+    echo $1
+
     cd $1 # $1 is first arg to function, not script
 
     if [ $USE_PROJECT_REF = "1" ]
@@ -46,10 +50,10 @@ update_and_run_project() {
     set_project_to_use_nuget
 }
 
+update_and_run_project "$EXAMPLES_DIR/ButtonSm1Cpp/CodeGen/"
+update_and_run_project "$EXAMPLES_DIR/LaserTagMenu1/CodeGen/"
 update_and_run_project "$EXAMPLES_DIR/Tutorial1-blank/CodeGen/"
 update_and_run_project "$EXAMPLES_DIR/Tutorial1-complete/CodeGen/"
-update_and_run_project "$EXAMPLES_DIR/LaserTagMenu1/CodeGen/"
 update_and_run_project "$EXAMPLES_DIR/BlankTemplate/CodeGen/"
 update_and_run_project "$EXAMPLES_DIR/Blinky1/CodeGen/"
 update_and_run_project "$EXAMPLES_DIR/Blinky1Printf/CodeGen/"
-update_and_run_project "$EXAMPLES_DIR/ButtonSm1Cpp/CodeGen/"
