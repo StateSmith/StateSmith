6abec14866853292f1e909a1ffde1bdbc00e5e73
Adam Fraser-Kruck <adamfk@gmail.com>
2024-04-28 22:22:44 -0500

    Added `LibVersionInfo` class to make it easier to get StateSmith version info.

---
+++ src/StateSmith/Output/LibVersionInfo.cs
@@ -0,0 +1,37 @@
+#nullable enable
+using System.Reflection;
+
+namespace StateSmith.Output;
+
+public class LibVersionInfo
+{
+    /// <summary>
+    /// Returns semantic version info for StateSmith library. Something like "0.9.10-alpha+12345".
+    /// The build information isn't always present.
+    /// </summary>
+    /// <param name="removeBuildInfo"></param>
+    /// <returns></returns>
+    public static string GetVersionInfoString(bool removeBuildInfo = false)
+    {
+        Assembly assembly = Assembly.GetExecutingAssembly();
+        AssemblyInformationalVersionAttribute? attr = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+
+        string versionInfo;
+
+        if (attr != null)
+        {
+            versionInfo = attr.InformationalVersion;
+            if (removeBuildInfo)
+            {
+                versionInfo = versionInfo.Split('+')[0];
+            }
+        }
+        else
+        {
+            System.Version? version = assembly.GetName().Version;
+            versionInfo = version?.ToString() + "-<unable-to-get-suffix>";
+        }
+
+        return versionInfo;
+    }
+}
