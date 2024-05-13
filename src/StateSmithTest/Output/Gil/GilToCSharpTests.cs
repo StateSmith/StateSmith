7a620c2bd2f70e9d0dafe6e5f608876b8306cd8d
Adam Fraser-Kruck <adamfk@gmail.com>
2024-05-12 21:02:56 -0400

    test - add integration test for nullable disable
    https://github.com/StateSmith/StateSmith/issues/231

--- src/StateSmithTest/Output/Gil/GilToCSharpTests.cs
+++ src/StateSmithTest/Output/Gil/GilToCSharpTests.cs
@@ -1,5 +1,9 @@
+using FluentAssertions;
+using StateSmith.Output;
 using StateSmith.Output.UserConfig;
 using StateSmith.Runner;
+using System.IO;
+using System.Linq;
 using Xunit;
 
 namespace StateSmithTest.Output.Gil;
@@ -12,15 +16,52 @@ public class GilToCSharpTests
     [Fact]
     public void DontRequireNameSpace()
     {
-        SmRunner runner = new(diagramPath: "CSharpNoNameSpaceExampleSm.drawio.svg", new MyGlueLogic(), transpilerId: TranspilerId.CSharp);
+        SmRunner runner = new(diagramPath: "CSharpNoNameSpaceExampleSm.drawio.svg", new DefaultCSharpRenderConfig(), transpilerId: TranspilerId.CSharp);
+        runner.Settings.outputDirectory = Path.GetTempPath();
         runner.GetExperimentalAccess().Settings.propagateExceptions = true;
         runner.Settings.outputStateSmithVersionInfo = false;
         //runner.GetExperimentalAccess().Settings.dumpGilCodeOnError = true;
         runner.Run();
     }
 
-    public class MyGlueLogic : IRenderConfigCSharp
+    public class DefaultCSharpRenderConfig : IRenderConfigCSharp
     {
         //string IRenderConfigCSharp.NameSpace => "MySmNs";
+    }
+
+    /// <summary>
+    /// https://github.com/StateSmith/StateSmith/issues/231
+    /// </summary>
+    [Fact]
+    public void TestNullableDisabled_231()
+    {
+        CapturingCodeFileWriter.Capture nonNullableFile = RenderAndCaptureOutput(new NullableDisableRenderConfig());
+        CapturingCodeFileWriter.Capture regNullableFile = RenderAndCaptureOutput(new DefaultCSharpRenderConfig());
+
+        regNullableFile.code.Should().Contain("#nullable enable");
+        nonNullableFile.code.Should().NotContain("#nullable enable");
+
+        regNullableFile.code.Should().Contain("currentStateExitHandler!(this)");
+        nonNullableFile.code.Should().Contain("currentStateExitHandler(this)");
+
+        regNullableFile.code.Should().Contain("private Func? ancestorEventHandler");
+        nonNullableFile.code.Should().Contain("private Func ancestorEventHandler");
+    }
+
+    private static CapturingCodeFileWriter.Capture RenderAndCaptureOutput(IRenderConfig renderConfig)
+    {
+        SmRunner runner = new(diagramPath: "CSharpNoNameSpaceExampleSm.drawio.svg", renderConfig, transpilerId: TranspilerId.CSharp);
+        runner.GetExperimentalAccess().Settings.propagateExceptions = true;
+        var fakeFileSystem = new CapturingCodeFileWriter();
+        runner.GetExperimentalAccess().DiServiceProvider.AddSingletonT<ICodeFileWriter>(fakeFileSystem);
+        runner.Run();
+
+        var file = fakeFileSystem.GetCapturesForFileName("CSharpNoNameSpaceExampleSm.cs").Single();
+        return file;
+    }
+
+    public class NullableDisableRenderConfig : IRenderConfigCSharp
+    {
+        bool IRenderConfigCSharp.UseNullable => false;
     }
 }
