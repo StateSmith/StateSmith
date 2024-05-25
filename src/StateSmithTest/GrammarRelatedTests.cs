4441b69de3d12a080c948284162f7e159f0cac96
Adam Fraser-Kruck <adamfk@gmail.com>
2024-05-25 00:23:18 -0500

    test - add more test coverage

--- src/StateSmithTest/GrammarRelatedTests.cs
+++ src/StateSmithTest/GrammarRelatedTests.cs
@@ -17,12 +17,18 @@ public class GrammarRelatedTests
     /// https://github.com/StateSmith/StateSmith/issues/277
     /// </summary>
     [Fact]
-    public void AllowExitInActionCode_277()
+    public void AllowExitInGuardActionCode_277()
     {
         var plantUmlText = """
             @startuml ExampleSm
             state c1 {
-                c1: SOME_EVENT / exit(); 
+                c1: [exit] / exit();
+                c1: event1 / exit();
+                c1: event1 / exit = 2;
+                c1: event1 / system.exit();
+                c1: event1 / EXIT();
+                c1: event1 / EXIT = 22;
+                c1: event1 / system.EXIT();
             }
             [*] --> c1
             @enduml
@@ -34,12 +40,16 @@ public class GrammarRelatedTests
     /// https://github.com/StateSmith/StateSmith/issues/277
     /// </summary>
     [Fact]
-    public void AllowEntryInActionCode_277()
+    public void AllowEntryInGuardActionCode_277()
     {
         var plantUmlText = """
             @startuml ExampleSm
             state c1 {
-                c1: SOME_EVENT / entry(); 
+                c1: [entry] / entry();
+                c1: event1 / entry();
+                c1: event1 / entry = 2;
+                c1: event1 / system.entry();
+                c1: event1 / ENTRY();
             }
             [*] --> c1
             @enduml
@@ -52,12 +62,13 @@ public class GrammarRelatedTests
     /// https://github.com/StateSmith/StateSmith/issues/211
     /// </summary>
     [Fact]
-    public void AllowViaInActionCode_211()
+    public void AllowViaInGuardActionCode_211()
     {
         var plantUmlText = """
 
             @startuml ExampleSm
             state c1 {
+                c1: [via] / via();
                 c1: event1 / via();
                 c1: event1 / via = 2;
                 c1: event1 / system.via();
