db53d1fd1f440bbfd779cc7547ea0282362b83bd
Mike Burton <mb-github@niskala.org>
2024-06-01 15:47:29 -0700

    toggle on click instead of add

--- src/StateSmith/Output/Sim/HtmlRenderer.cs
+++ src/StateSmith/Output/Sim/HtmlRenderer.cs
@@ -258,7 +258,7 @@ public class HtmlRenderer
         /* When the user clicks on the button, 
         toggle between hiding and showing the dropdown content */
         function myFunction() {
-          document.getElementById('myDropdown').classList.add('show');
+          document.getElementById('myDropdown').classList.toggle('show');
         }
 
         // Close the dropdown if the user clicks outside of it
