869a5b45ae83abe647e1c0b28c39008f69e3f1a5
Mike Burton <mb-github@niskala.org>
2024-08-03 18:16:04 -0400

    specify that --lang is required, and a little cleanup

--- src/StateSmith.Cli/Run/RunLiteOptions.cs
+++ src/StateSmith.Cli/Run/RunLiteOptions.cs
@@ -12,21 +12,21 @@ public class RunLiteOptions
 {
     public const string Description = "Run StateSmith code generation.";
 
-    [Option(HelpText = "Specifies programming language for transpiler. Ignored for csx files.")]
+    [Option(Required=true, HelpText = "Specifies programming language for transpiler. Ignored for csx files.")]
     public TranspilerId Lang { get; set; } = TranspilerId.NotYetSet;
-
-    [Value(0)]
-    public IList<string> Files { get; set; } = new List<string>();
-
-
-    [Option("no-sim-gen", HelpText = "Disables simulation .html file generation. Ignored for csx files.")]
-    public bool NoSimGen { get; set; } = false;
 
     [Option('w', "watch", HelpText = "Watch for changes. Continue watching for changes after initial run and reprocess any files as they change.")]
     public bool Watch { get; set; } = false;
 
     [Option('v', "verbose", HelpText = "Enables verbose info printing.")]
     public bool Verbose { get; set; } = false;
+
+    [Option("no-sim-gen", HelpText = "Disables simulation .html file generation. Ignored for csx files.")]
+    public bool NoSimGen { get; set; } = false;
+
+    [Value(0)]
+    public IList<string> Files { get; set; } = new List<string>();
+
 
     public DiagramOptions GetDiagramOptions()
     {
