6c4c9e5e9a3dfa9dc36d22a13a99af5039a02437
Mike Burton <mb-github@niskala.org>
2025-03-11 07:43:02 -0700

    fix import

--- docs/languages/java/MyApp.java
+++ docs/languages/java/MyApp.java
@@ -1,10 +1,7 @@
-import Lightbulb;
-
 public class MyApp {
     public static void main(String[] args) {        
         Lightbulb bulb = new Lightbulb();
         bulb.start();
         bulb.dispatchEvent(Lightbulb.Events.SWITCH);
-
     }
 }
