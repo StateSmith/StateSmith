1010d69fedf7f0fe99f54711f9ed624b8d16a383
Mike Burton <mb-github@niskala.org>
2025-03-13 07:10:47 -0700

    fixes

--- docs/languages/cpp/LightbulbCallback.hpp
+++ docs/languages/cpp/LightbulbCallback.hpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#pragma once
+
 using namespace std;
 
 /* Implement a class for your statemachine that contains your callbacks */
 class LightbulbCallback {
-protected:
+public:
     void enter_on() { cout << "Lightbulb is on." << endl; }
     void enter_off() { cout << "Lightbulb is off." << endl; }
 };
