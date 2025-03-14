eeef998cdd191fcbb28580fe454681486a4dabe6
Mike Burton <mb-github@niskala.org>
2025-03-14 08:35:58 -0700

    loop

---
+++ docs/languages/python/myapp.py
@@ -0,0 +1,18 @@
+from lightbulb_callback import LightbulbCallback
+from lightbulb import lightbulb
+
+def main():
+    callback = LightbulbCallback()
+    bulb = lightbulb(callback)
+    bulb.start()
+
+    print("Press <enter> to toggle the light switch.")
+    print("Press ^C to quit.")
+
+    while True:
+        command = input()
+        bulb.dispatchEvent(lightbulb.EventId.SWITCH)
+
+
+if __name__ == "__main__":
+    main()
