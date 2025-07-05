6bd5c41f91caf27d5ec1e38400a30eec099fafca
Mike Burton <mb-github@niskala.org>
2025-07-05 13:43:34 -0700

    one more using

--- src/StateSmith.Cli/Run/DiagramRunner.cs
+++ src/StateSmith.Cli/Run/DiagramRunner.cs
@@ -106,11 +106,12 @@ public class DiagramRunner
         runInfoStore.diagramRuns[absolutePath] = info; // will overwrite if already exists
 
         // the constructor will attempt to read diagram settings from the diagram file
-        var spBuilder = DefaultServiceProviderBuilder.CreateDefault((services) =>
+        using var spBuilder = DefaultServiceProviderBuilder.CreateDefault((services) =>
         {
             services.AddSingleton<ICodeFileWriter, LoggingCodeFileWriter>();
-        });
-        SmRunner smRunner = new(settings: runnerSettings, renderConfig: null, callerFilePath: callerFilePath, serviceProviderBuilder: spBuilder);
+        });        
+        
+        SmRunner smRunner = new(settings: runnerSettings, renderConfig: null, callerFilePath: callerFilePath, serviceProvider: spBuilder.Build());
 
         if (smRunner.PreDiagramBasedSettingsException != null)
         {
