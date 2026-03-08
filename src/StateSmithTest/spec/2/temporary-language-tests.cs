ee6d59f65bbc844227f7fc6763217ae6b6fb3d87
Adam Fraser-Kruck <adamfk@gmail.com>
2026-03-08 17:39:58 -0500

    lib - github - add support for pipeline to build and run Kotlin and Swift tests.

---
+++ src/StateSmithTest/spec/2/temporary-language-tests.cs
@@ -0,0 +1,62 @@
+using StateSmithTest.Processes;
+using Xunit;
+
+// This file is temporary.
+// Will remove once we have swift MR merged
+
+namespace StateSmithTest.spec2.fake;
+
+public abstract class FakeSpec2Tests
+{
+    public FakeSpec2Tests()
+    {
+        Setup();
+    }
+
+    public virtual void Setup()
+    {
+
+    }
+
+    [Fact]
+    public void AssertNoCrash()
+    {
+        Assert.Equal(1, 1);
+    }
+}
+
+public class Spec2TestsSwift : FakeSpec2Tests
+{
+    public override void Setup()
+    {
+        SimpleProcess process = new()
+        {
+            ProgramPath = "swiftc",
+            Args = " --version",
+            throwOnStdErr = false   // required for github runner
+        };
+        process.Run(timeoutMs: SimpleProcess.DefaultLongTimeoutMs);
+
+        process = new()
+        {
+            ProgramPath = "swift",
+            Args = " --version",
+            throwOnStdErr = false   // required for github runner
+        };
+        process.Run(timeoutMs: SimpleProcess.DefaultLongTimeoutMs);
+    }
+}
+
+public class Spec2TestsKotlin : FakeSpec2Tests
+{
+    public override void Setup()
+    {
+        SimpleProcess process = new()
+        {
+            ProgramPath = "kotlinc",
+            Args = " -version",
+            throwOnStdErr = false   // `kotlinc -version` prints to stderr so this is normal
+        };
+        process.Run(timeoutMs: SimpleProcess.DefaultLongTimeoutMs);
+    }
+}
