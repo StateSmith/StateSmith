035cec9eceb57978134ffa285762015b54661e2e
Adam Fraser-Kruck <adamfk@gmail.com>
2026-04-12 14:56:33 -0500

    lib - test - update AlgoBalanced1SettingsTests for #535.

--- src/StateSmithTest/Output/BalancedCoder1/AlgoBalanced1SettingsTests.cs
+++ src/StateSmithTest/Output/BalancedCoder1/AlgoBalanced1SettingsTests.cs
@@ -7,54 +7,60 @@ using Xunit;
 
 namespace StateSmithTest.Output.BalancedCoder1;
 
+// https://github.com/StateSmith/StateSmith/issues/181
+// https://github.com/StateSmith/StateSmith/issues/535
 public class AlgoBalanced1SettingsTests
 {
     /// <summary>
     /// Captures what would normally be written to file to memory instead.
     /// </summary>
     readonly CapturingCodeFileWriter capturedFile = new();
+    private SmRunner runner;
 
-    // https://github.com/StateSmith/StateSmith/issues/181
-    [Fact]
-    public void NormalBehaviorHasToStringFunctions()
+    private const string StateIdToString = "StateIdToString";
+    private const string EventIdToString = "EventIdToString";
+    private const string GetParentId = "GetParentId";
+
+    public AlgoBalanced1SettingsTests()
     {
-        SmRunner runner = new(diagramPath: "ExBc1.drawio", transpilerId:TranspilerId.CSharp);
-        SetupForUnitTest(capturedFile, runner);
-        runner.Run();
-
-        capturedFile.LastCode.Should().Contain("public static string StateIdToString(");
-        capturedFile.LastCode.Should().Contain("public static string EventIdToString(");
-    }
-
-    // https://github.com/StateSmith/StateSmith/issues/181
-    [Fact]
-    public void RemoveEventIdToString()
-    {
-        SmRunner runner = new(diagramPath: "ExBc1.drawio", transpilerId: TranspilerId.CSharp);
-        SetupForUnitTest(capturedFile, runner);
-        runner.Settings.algoBalanced1.outputEventIdToStringFunction = false; // Here's the setting you want
-        runner.Run();
-
-        capturedFile.LastCode.Should().Contain("public static string StateIdToString");
-        capturedFile.LastCode.Should().NotContain("public static string EventIdToString(");
-    }
-
-    // https://github.com/StateSmith/StateSmith/issues/181
-    [Fact]
-    public void RemoveStateIdToString()
-    {
-        SmRunner runner = new(diagramPath: "ExBc1.drawio", transpilerId: TranspilerId.CSharp);
-        SetupForUnitTest(capturedFile, runner);
-        runner.Settings.algoBalanced1.outputStateIdToStringFunction = false; // Here's the setting you want
-        runner.Run();
-
-        capturedFile.LastCode.Should().NotContain("public static string StateIdToString");
-        capturedFile.LastCode.Should().Contain("public static string EventIdToString(");
-    }
-
-    private static void SetupForUnitTest(CapturingCodeFileWriter capturedFile, SmRunner runner)
-    {
+        runner = new SmRunner(diagramPath: "ExBc1.drawio", transpilerId:TranspilerId.CSharp);
         runner.GetExperimentalAccess().DiServiceProvider.AddSingletonT<ICodeFileWriter>(capturedFile);
         runner.Settings.propagateExceptions = true;
+    }
+
+    [Fact]
+    public void DefaultBehavior()
+    {
+        runner.Run();
+
+        capturedFile.LastCode.Should().Contain($"public static string {StateIdToString}(");
+        capturedFile.LastCode.Should().Contain($"public static string {EventIdToString}(");
+        capturedFile.LastCode.Should().Contain($"public static StateId {GetParentId}(");
+    }
+
+    [Fact]
+    public void DisableAll()
+    {
+        runner.Settings.algoBalanced1.outputEventIdToStringFunction = false;
+        runner.Settings.algoBalanced1.outputStateIdToStringFunction = false;
+        runner.Settings.algoBalanced1.outputGetParentIdFunction = false;
+        runner.Run();
+
+        capturedFile.LastCode.Should().NotContain($"public static string {StateIdToString}(");
+        capturedFile.LastCode.Should().NotContain($"public static string {EventIdToString}(");
+        capturedFile.LastCode.Should().NotContain($"public static StateId {GetParentId}(");
+    }
+
+    [Fact]
+    public void EnableAll()
+    {
+        runner.Settings.algoBalanced1.outputEventIdToStringFunction = true;
+        runner.Settings.algoBalanced1.outputStateIdToStringFunction = true;
+        runner.Settings.algoBalanced1.outputGetParentIdFunction = true;
+        runner.Run();
+
+        capturedFile.LastCode.Should().Contain($"public static string {StateIdToString}(");
+        capturedFile.LastCode.Should().Contain($"public static string {EventIdToString}(");
+        capturedFile.LastCode.Should().Contain($"public static StateId {GetParentId}(");
     }
 }
